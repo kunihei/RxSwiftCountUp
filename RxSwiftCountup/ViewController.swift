db4c22a629586e6ebd197a0b2068c8500838561f
kunihei <57933493+kunihei@users.noreply.github.com>
2021-11-15 21:32:14 +0900

    RxSwiftを使用して簡単なカウントアップアプリを作成しました

--- RxSwiftCountup/ViewController.swift
+++ RxSwiftCountup/ViewController.swift
@@ -6,11 +6,26 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var favCountLabel: UILabel!
+    @IBOutlet weak var favButton: UIButton!
+    
+    private var favCount = 0
+    private let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        favButton.rx.tap
+            .subscribe { [unowned self] _ in
+                self.favCount += 1
+                self.favCountLabel.text = String(self.favCount)
+            }.disposed(by: disposeBag)
+
+        
         // Do any additional setup after loading the view.
     }
 
